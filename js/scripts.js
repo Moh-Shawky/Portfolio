6d73fae7d9280c2b6975d94a846d4578d79d05da
Moh Shawky <mohamedshawky20m@gmail.com>
2023-11-02 04:29:17 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+/*!
+* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+
+const link = document.getElementById('contact-but');
+link.classList.add('disabled');
+const linkk = document.getElementById('contact-butt');
+linkk.classList.add('disabled');
